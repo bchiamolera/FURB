fd672a960b066841ca190b8fdaf08ea9434e37a0
bchiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-03-15 10:29:21 -0300

    Update Uni3Exe01.java

--- IP/Unidade3/src/Uni3Exe01.java
+++ IP/Unidade3/src/Uni3Exe01.java
@@ -1,6 +1,7 @@
-/* Uma imobiliária vende apenas terrenos retangulares.
- * Faça um programa para ler as dimensões de um terreno
- * e depois exibir a área do terreno.
+/*
+Uma imobiliária vende apenas terrenos retangulares.
+Faça um programa para ler as dimensões de um terreno
+e depois exibir a área do terreno.
  */
 
 import java.util.Scanner;
