91660b243b221880cbf832764cad5ea6302011a0
bchiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-03-15 10:41:52 -0300

    Update Uni3Exe16.java

--- IP/Unidade3/src/Uni3Exe16.java
+++ IP/Unidade3/src/Uni3Exe16.java
@@ -1,3 +1,12 @@
+/*
+Suponha que um caixa disponha apenas de notas de 100, 10 e 1 reais.
+Considerando que alguém está pagando uma compra, faça um programa
+que determine e escreva o número mínimo de notas que o caixa deve
+fornecer como troco. Escreva também o número de cada tipo de nota
+a ser fornecido como troco. Suponha que o sistema monetário não
+utilize centavos.
+ */
+
 import java.util.Scanner;
 
 public class Uni3Exe16 {
