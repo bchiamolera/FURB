b2944afb7508df86cd09877ae14e73ba114a06fd
bchiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-03-15 10:31:26 -0300

    Update Uni3Exe05.java

--- IP/Unidade3/src/Uni3Exe05.java
+++ IP/Unidade3/src/Uni3Exe05.java
@@ -1,3 +1,11 @@
+/*
+Uma granja possui um controle automatizado de cada frango da sua produção.
+No pé direito do frango há um anel com um chip de identificação; no pé esquerdo
+são dois anéis para indicar o tipo de alimento que ele deve consumir. Sabendo
+que o anel com chip custa R$ 4,00 e o anel de alimento custa R$ 3,50, faça um
+programa para calcular o gasto total da granja para marcar todos os seus frangos.
+ */
+
 import java.util.Scanner;
 
 public class Uni3Exe05 {
