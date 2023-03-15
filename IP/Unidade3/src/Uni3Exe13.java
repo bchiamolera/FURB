a5bb6330a51144df6e51f39d2939bade1d231a2f
bchiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-03-15 10:39:28 -0300

    Update Uni3Exe13.java

--- IP/Unidade3/src/Uni3Exe13.java
+++ IP/Unidade3/src/Uni3Exe13.java
@@ -1,3 +1,10 @@
+/*
+Numa loja de materiais de construção, um azulejo estampado custa R$ 12,50.
+Faça um programa para ler o comprimento e altura de uma parede (em metros),
+e depois escrever o valor gasto com a compra de azulejos. Considere que um
+metro quadrado é formado por 9 azulejos.
+ */
+
 import java.util.Scanner;
 
 public class Uni3Exe13 {
