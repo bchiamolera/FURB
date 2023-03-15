84664e59aa1bb54d9ae47c82d00cf8dd163a808b
bchiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-03-15 10:41:04 -0300

    Update Uni3Exe15.java

--- IP/Unidade3/src/Uni3Exe15.java
+++ IP/Unidade3/src/Uni3Exe15.java
@@ -1,3 +1,14 @@
+/*
+Construa um programa para ler um número inteiro (assuma até 3 dígitos)
+e imprima a saída da seguinte forma:
+
+X centena(s)  Y dezena(s) K unidade(s)
+
+Exemplo, se for submetido o número 384, o programa deverá exibir:
+
+3 centena(s)  8 dezena(s) 4 unidade(s)
+ */
+
 import java.util.Scanner;
 
 public class Uni3Exe15 {
