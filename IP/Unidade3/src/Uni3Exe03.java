d19bc8068ca710789a86260607e912058709bae1
bchiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-03-15 10:30:00 -0300

    Update Uni3Exe03.java

--- IP/Unidade3/src/Uni3Exe03.java
+++ IP/Unidade3/src/Uni3Exe03.java
@@ -1,3 +1,9 @@
+/*
+Um motorista deseja abastecer seu tanque de combustível. Escreva um programa
+para ler o preço do litro da gasolina e o valor do pagamento e exibir quantos
+litros ele conseguiu colocar no tanque.
+ */
+
 import java.util.Scanner;
 
 public class Uni3Exe03 {
