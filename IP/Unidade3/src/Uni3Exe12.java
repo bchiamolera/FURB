e6cdd1a17bdf264ca1fda31d3f51381d7c0699cb
bchiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-03-15 10:38:41 -0300

    Update Uni3Exe12.java

--- IP/Unidade3/src/Uni3Exe12.java
+++ IP/Unidade3/src/Uni3Exe12.java
@@ -1,3 +1,14 @@
+/*
+Uma empresa tem para um funcionário os seguintes dados: o nome,
+o número de horas trabalhadas mensais e o número de dependentes.
+A empresa paga R$ 10,00 por hora (valor para cálculo do salário
+trabalho) e R$ 60,00 por dependente (valor para cálculo do salário
+família) e são feitos descontos de 8,5% sobre o salário trabalho
+para o INSS e de 5% sobre o salário trabalho para o imposto de renda.
+Descreva um programa que informe o nome, o salário bruto e o salário
+líquido do funcionário.
+ */
+
 import java.util.Scanner;
 
 public class Uni3Exe12 {
