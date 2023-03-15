1e205e456f9eca9e7442ec5694d40152e991a65a
bchiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-03-15 10:29:03 -0300

    Update Uni3Exe02.java

--- IP/Unidade3/src/Uni3Exe02.java
+++ IP/Unidade3/src/Uni3Exe02.java
@@ -1,3 +1,14 @@
+/*
+Uma loja de calçados está concedendo 12% de desconto nos produtos.
+Escreva um programa para calcular e exibir o valor de desconto a ser
+dado num par de sapatos e quanto deve custar o produto com o desconto.
+O preço do par de sapatos deve ser informado pelo usuário. Como resultado,
+o programa deverá exibir as seguintes mensagens:
+
+O valor do desconto é de R$ xxx
+O preço do par de sapatos com desconto é R$ xxx
+ */
+
 import java.util.Scanner;
 
 public class Uni3Exe02 {
