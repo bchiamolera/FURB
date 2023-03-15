e390cd7d157e1314e945ef72890ec68d3cdd02df
bchiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-03-15 10:40:14 -0300

    Update Uni3Exe14.java

--- IP/Unidade3/src/Uni3Exe14.java
+++ IP/Unidade3/src/Uni3Exe14.java
@@ -1,3 +1,10 @@
+/*
+Descreva um programa que a partir da distância percorrida e o
+do tempo gasto por um motorista durante uma viagem de final de
+semana, calcule a velocidade média e a quantidade de combustível
+gasto na viagem, sabendo que o automóvel faz 12 km por litro.
+ */
+
 import java.util.Scanner;
 
 public class Uni3Exe14 {
