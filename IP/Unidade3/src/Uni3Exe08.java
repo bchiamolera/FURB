b9f3175eab3913fedf8ac120aa5532c3e5d706cb
bchiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-03-15 10:33:52 -0300

    Update Uni3Exe08.java

--- IP/Unidade3/src/Uni3Exe08.java
+++ IP/Unidade3/src/Uni3Exe08.java
@@ -1,3 +1,10 @@
+/*
+Uma pessoa foi até uma casa de câmbio trocar dólares por reais.
+Para isto ela entregou um valor em dólares para o atendente.
+Considerando que o atendente tem a cotação do dólar, descreva um
+programa para calcular quantos reais o atendente deve devolver para a pessoa.
+ */
+
 import java.util.Scanner;
 
 public class Uni3Exe08 {
