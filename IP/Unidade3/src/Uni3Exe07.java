a9ea6b7c5c2292b6e5dd8a754e078e5691a9f13e
bchiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-03-15 10:32:57 -0300

    Update Uni3Exe07.java

--- IP/Unidade3/src/Uni3Exe07.java
+++ IP/Unidade3/src/Uni3Exe07.java
@@ -1,3 +1,10 @@
+/*
+Uma fábrica de refrigerantes vende seu produto em três formatos:
+lata de 350 ml, garrafa de 600 ml e garrafa de 2 litros.
+Se um comerciante compra uma determinada quantidade de cada formato,
+faça um programa para calcular quantos litros de refrigerante ele comprou.
+ */
+
 import java.util.Scanner;
 
 public class Uni3Exe07 {
