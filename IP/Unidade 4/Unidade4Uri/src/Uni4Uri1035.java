6f3decdc396215c79669e2b53296c6c694c74062
Bernardo Chiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-04-03 10:34:24 -0300

    Add files via upload

---
+++ IP/Unidade 4/Unidade4Uri/src/Uni4Uri1035.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Uni4Uri1035 {
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+
+        int A = teclado.nextInt();
+        int B = teclado.nextInt();
+        int C = teclado.nextInt();
+        int D = teclado.nextInt();
+
+        if (B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0 && (A % 2) == 0) {
+            System.out.println("Valores aceitos");
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
+
+        teclado.close();
+    }
+}
