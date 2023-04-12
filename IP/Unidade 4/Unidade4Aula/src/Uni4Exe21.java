776151410a5950fc8b2185d249eb9c9f29fc1b43
Bernardo Chiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-04-12 08:06:12 -0300

    Update Uni4Exe21.java

--- IP/Unidade 4/Unidade4Aula/src/Uni4Exe21.java
+++ IP/Unidade 4/Unidade4Aula/src/Uni4Exe21.java
@@ -10,13 +10,13 @@ IMC= Massa / Altura^2
 Faça um algoritmo para classificar o IMC e dizer o grau de
 obesidade do indivíduo, de acordo com a seguinte tabela:
 ..................................................
-|     IMC	    |          Classificação         |
-|  > 18.5	    |  Magreza                       |
-|  18.5 - 25	|  Saudável                      |
-|  25.0 - 29.9	|  Sobrepeso                     |
-|  30.0 - 34.9	|  Obesidade Grau I              |
-|  35.0 - 39.9	|  Obesidade Grau II (severa)    |
-|  >= 40.0	    |  Obesidade Grau III (mórbida)  |
+|     IMC       |          Classificação         |
+|  < 18.5       |  Magreza                       |
+|  18.5 - 24.9  |  Saudável                      |
+|  25.0 - 29.9  |  Sobrepeso                     |
+|  30.0 - 34.9  |  Obesidade Grau I              |
+|  35.0 - 39.9  |  Obesidade Grau II (severa)    |
+|  >= 40.0      |  Obesidade Grau III (mórbida)  |
 ..................................................
  */
 
