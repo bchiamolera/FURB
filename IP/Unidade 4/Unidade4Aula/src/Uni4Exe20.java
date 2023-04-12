3e312dcfdb4d2f8088a9f1cc687eef9c5a4e196e
Bernardo Chiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-04-12 08:04:21 -0300

    Update Uni4Exe20.java

--- IP/Unidade 4/Unidade4Aula/src/Uni4Exe20.java
+++ IP/Unidade 4/Unidade4Aula/src/Uni4Exe20.java
@@ -8,11 +8,11 @@ media = (notaProva1 + notaProva2 * 2 + notaProva3 * 3 + notaExercicios) / 7
 A atribuição dos conceitos obedece à tabela abaixo:
 ..................................
 |       Media	    |  Conceito  |
-| >= 9.0	        |     A      |
-| >= 7.5 e < 9.0	|     B      |
-| >= 6.0 e < 7.5	|     C      |
-| >= 4.0 e < 6.0	|     D      |
-| < 4.0             |     E      |
+|  >= 9.0           |     A      |
+|  >= 7.5 e < 9.0   |     B      |
+|  >= 6.0 e < 7.5   |     C      |
+|  >= 4.0 e < 6.0   |     D      |
+|  < 4.0            |     E      |
 ..................................
 
 O algoritmo deve escrever a média de aproveitamento, o conceito correspondente e
