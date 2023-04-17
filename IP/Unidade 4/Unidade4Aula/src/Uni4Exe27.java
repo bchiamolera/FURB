e635a90021f9a5c8cb386337f27bd32378d19970
Bernardo Chiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-04-17 10:41:55 -0300

    Update Uni4Exe27.java

--- IP/Unidade 4/Unidade4Aula/src/Uni4Exe27.java
+++ IP/Unidade 4/Unidade4Aula/src/Uni4Exe27.java
@@ -32,80 +32,85 @@ public class Main {
         System.out.println("Escolha o horário de chegada (Ex.: 12 50): ");
         int horaChegada = teclado.nextInt();
         int minutosChegada = teclado.nextInt();
-        int totalChegada = horaChegada * 60 + minutosChegada;
+        if (horaChegada >= 0 && horaChegada < 24 && minutosChegada >= 0 && minutosChegada <= 59) {
+            int totalChegada = horaChegada * 60 + minutosChegada;
 
-        System.out.println("Escolha o horário de saída: (Ex: 20 10)");
-        int horaSaida = teclado.nextInt();
-        int minutosSaida = teclado.nextInt();
-        int totalSaida = horaSaida * 60 + minutosSaida;
+            System.out.println("Escolha o horário de saída: (Ex: 20 10)");
+            int horaSaida = teclado.nextInt();
+            int minutosSaida = teclado.nextInt();
+            if (horaSaida >= 0 && horaSaida < 24 && minutosSaida >= 0 && minutosSaida <= 59) {
+                int totalSaida = horaSaida * 60 + minutosSaida;
 
-        if (totalChegada >= 0 && totalSaida <= 1439 && totalSaida > totalChegada) {
-            int tempoTotal = totalSaida - totalChegada;
-            int tarifa = 0;
-            
-        if (tempoTotal < 150) {
-                tarifa = 1;
-            } else if (tempoTotal >= 150 && tempoTotal < 270) {
-                tarifa = 2;
-            } else if (tempoTotal >= 270) {
-                tarifa = 3;
-            }
+                if (totalChegada >= 0 && totalSaida <= 1439 && totalSaida > totalChegada) {
+                    int tempoTotal = totalSaida - totalChegada;
+                    int tarifa = 0;
 
-            int horasTotal = tempoTotal / 60;
-            int minutosTotal = tempoTotal % 60;
-            Double preçoTarifa = 0.0;
-            Double valorTotal = 0.0;
-            int horaCobrada = 0;
-
-            switch (tarifa) {
-                case 1: {
-                    preçoTarifa = 5.0;
-                    if ((minutosTotal < 30 && horasTotal == 0) || minutosTotal >= 30) {
-                        valorTotal = (horasTotal + 1) * preçoTarifa;
-                        horaCobrada = horasTotal + 1;
-                    } else {
-                        valorTotal = horasTotal * preçoTarifa;
-                        horaCobrada = horasTotal;
+                    if (tempoTotal < 150) {
+                        tarifa = 1;
+                    } else if (tempoTotal >= 150 && tempoTotal < 270) {
+                        tarifa = 2;
+                    } else if (tempoTotal >= 270) {
+                        tarifa = 3;
                     }
-                    System.out.println("Tempo estacionado: " + horasTotal + "h " + minutosTotal + "min");
-                    System.out.println("Tempo cobrado: " + horaCobrada + "h " + "00min");
-                    System.out.println("Preço Tarifa = R$5.00 / h (1ª e 2ª hora)");
-                    System.out.println("Valor total = R$" + df_02.format(valorTotal));
-                    break;
-                }
-                case 2: {
-                    preçoTarifa = 7.5;
-                    if (minutosTotal >= 30) {
-                        valorTotal = 10 + (horasTotal + 1 - 2) * preçoTarifa;
-                        horaCobrada = horasTotal + 1;
-                    } else {
-                        valorTotal = 10 + (horasTotal - 2) * preçoTarifa;
-                        horaCobrada = horasTotal;
+
+                    int horasTotal = tempoTotal / 60;
+                    int minutosTotal = tempoTotal % 60;
+                    Double preçoTarifa = 0.0;
+                    Double valorTotal = 0.0;
+                    int horaCobrada = 0;
+
+                    switch (tarifa) {
+                        case 1: {
+                            preçoTarifa = 5.0;
+                            if ((minutosTotal < 30 && horasTotal == 0) || minutosTotal >= 30) {
+                                valorTotal = (horasTotal + 1) * preçoTarifa;
+                                horaCobrada = horasTotal + 1;
+                            } else {
+                                valorTotal = horasTotal * preçoTarifa;
+                                horaCobrada = horasTotal;
+                            }
+                            System.out.println("Tempo estacionado: " + horasTotal + "h " + minutosTotal + "min");
+                            System.out.println("Tempo cobrado: " + horaCobrada + "h " + "00min");
+                            System.out.println("Preço Tarifa = R$5.00 / h (1ª e 2ª hora)");
+                            System.out.println("Valor total = R$" + df_02.format(valorTotal));
+                            break;
+                        }
+                        case 2: {
+                            preçoTarifa = 7.5;
+                            if (minutosTotal >= 30) {
+                                valorTotal = 10 + (horasTotal + 1 - 2) * preçoTarifa;
+                                horaCobrada = horasTotal + 1;
+                            } else {
+                                valorTotal = 10 + (horasTotal - 2) * preçoTarifa;
+                                horaCobrada = horasTotal;
+                            }
+                            System.out.println("Tempo estacionado: " + horasTotal + "h " + minutosTotal + "min");
+                            System.out.println("Tempo cobrado: " + horaCobrada + "h " + "00min");
+                            System.out.println("Preço Tarifa = R$10 + R$7.50 / h (3ª e 4ª hora)");
+                            System.out.println("Valor total = R$" + df_02.format(valorTotal));
+                            break;
+                        }
+                        case 3: {
+                            preçoTarifa = 10.0;
+                            if (minutosTotal >= 30) {
+                                valorTotal = 25 + (horasTotal + 1 - 4) * preçoTarifa;
+                                horaCobrada = horasTotal + 1;
+                            } else {
+                                valorTotal = 25 + (horasTotal - 4) * preçoTarifa;
+                                horaCobrada = horasTotal;
+                            }
+                            System.out.println("Tempo estacionado: " + horasTotal + "h " + minutosTotal + "min");
+                            System.out.println("Tempo cobrado: " + horaCobrada + "h " + "00min");
+                            System.out.println("Preço Tarifa = R$25.00 + R$10.00 / h (5ª hora)");
+                            System.out.println("Valor total = R$" + df_02.format(valorTotal));
+                            break;
+                        }
                     }
-                    System.out.println("Tempo estacionado: " + horasTotal + "h " + minutosTotal + "min");
-                    System.out.println("Tempo cobrado: " + horaCobrada + "h " + "00min");
-                    System.out.println("Preço Tarifa = R$10 + R$7.50 / h (3ª e 4ª hora)");
-                    System.out.println("Valor total = R$" + df_02.format(valorTotal));
-                    break;
+                } else {
+                    System.out.println("Valor(es) inválido(s). Entrada e saída devem ser no mesmo dia.");
                 }
-                case 3: {
-                    preçoTarifa = 10.0;
-                    if (minutosTotal >= 30) {
-                        valorTotal = 25 + (horasTotal + 1 - 4) * preçoTarifa;
-                        horaCobrada = horasTotal + 1;
-                    } else {
-                        valorTotal = 25 + (horasTotal - 4) * preçoTarifa;
-                        horaCobrada = horasTotal;
-                    }
-                    System.out.println("Tempo estacionado: " + horasTotal + "h " + minutosTotal + "min");
-                    System.out.println("Tempo cobrado: " + horaCobrada + "h " + "00min");
-                    System.out.println("Preço Tarifa = R$25.00 + R$10.00 / h (5ª hora)");
-                    System.out.println("Valor total = R$" + df_02.format(valorTotal));
-                    break;
-                }
-                default: {
-                    System.out.println("Valor(es) inválido(s)");
-                }
+            } else {
+                System.out.println("Valor(es) inválido(s)");
             }
         } else {
             System.out.println("Valor(es) inválido(s)");
