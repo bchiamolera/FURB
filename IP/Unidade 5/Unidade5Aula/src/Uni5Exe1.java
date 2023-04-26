c4051cb94ceb7d512b81eccbc7b0ee7c22f50372
Bernardo Chiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-04-26 11:05:57 -0300

    Delete Uni5Exe1.java

--- IP/Unidade 5/Unidade5Aula/src/Uni5Exe1.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Uni5Exe1 {
-    public static void main(String[] args) throws Exception {
-        Scanner teclado = new Scanner(System.in);
-        teclado.close();
-    }
-}
