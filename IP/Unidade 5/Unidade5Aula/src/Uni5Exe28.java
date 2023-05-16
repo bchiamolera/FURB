92429874abc2eb556c374ed00d0481783c2510b7
Bernardo <bernardo.ch.be@gmail.com>
2023-05-16 19:29:30 -0300

    .

---
+++ IP/Unidade 5/Unidade5Aula/src/Uni5Exe28.java
@@ -0,0 +1,25 @@
+/*
+ Uma emissora de rádio decidiu promover uma eleição para determinar o melhor conjunto do ano segundo a
+ opinião dos ouvintes. Entraram na disputa apenas os quatro conjuntos mais solicitados durante o ano. Os
+ votos são informados através de códigos:
+
+ código	| conjunto
+ 1	    | Nenhum de Nós
+ 2	    | CPM22
+ 3	    | Skank
+ 4	    | Jota Quest
+
+ Descreva um algoritmo que:
+ - informe o total de votos para cada grupo;
+ - informe o percentual dos votos para cada grupo;
+ - informe o grupo vencedor.
+
+ O algoritmo deve apresentar ao usuário a seguinte mensagem: mais um voto: s (SIM) / n (NÃO)? antes de
+ prosseguir com a entrada de dados.
+ */
+
+public class Uni5Exe28 {
+    public static void main(String[] args) {
+        int totalVotos = 0, total1 = 0, total2 = 0, total3 = 0, total4 = 0;
+    }
+}
