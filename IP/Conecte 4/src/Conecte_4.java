5fcc3a5e1bda35475639cbeb43ab816cfcc7e3cb
Bernardo Chiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-06-12 10:32:15 -0300

    Update Conecte_4.java

--- IP/Conecte 4/src/Conecte_4.java
+++ IP/Conecte 4/src/Conecte_4.java
@@ -8,14 +8,24 @@ public class Conecte_4 {
     public Conecte_4() {
         Scanner teclado = new Scanner(System.in);
 
-        char[][] tabuleiro = new char[6] [7];
-        criacao(tabuleiro);
-        output(tabuleiro);
+        char[][] tabuleiro = new char[6][7];
+        char jogador = ' ';
+        char computador = ' ';
+
+        Criacao(tabuleiro);
+        jogador = Escolha(teclado);
+        if (jogador ==  'A') {
+            computador =  'V';
+        } else {
+            computador = 'A';
+        }
+
+        Output(tabuleiro);
 
         teclado.close();
     }
 
-    private void criacao(char[][] tabuleiro) {
+    private void Criacao(char[][] tabuleiro) {
         for (int linhas = 0; linhas < 6; linhas++) {
             for (int colunas = 0; colunas < 7; colunas++) {
                 tabuleiro[linhas][colunas] = 'B';
@@ -23,10 +33,24 @@ public class Conecte_4 {
         }
     }
 
-    private void output(char[][] tabuleiro) {
+    private char Escolha(Scanner teclado) {
+        char op = ' ';
+        boolean check = false;
+        do {
+            System.out.println("Escolha a sua cor: A - Amarelo; V - Vermelho");
+            op = teclado.next().trim().toUpperCase().charAt(0);
+            if (op == 'A' || op == 'V') {
+                check = true;
+            }
+        } while (!check);
+        return op;
+    }
+
+    private void Output(char[][] tabuleiro) {
         for (int linhas = 0; linhas < 6; linhas++) {
             for (int colunas = 0; colunas < 7; colunas++) {
-                System.out.print(tabuleiro[linhas][colunas] + " ");;
+                System.out.print(tabuleiro[linhas][colunas] + " ");
+                ;
             }
             System.out.println();
         }
