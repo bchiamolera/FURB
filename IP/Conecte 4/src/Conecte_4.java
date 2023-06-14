88790f4c8810b9ed687029d6e8e610f873ce52f5
Bernardo Chiamolera <120071237+bchiamolera@users.noreply.github.com>
2023-06-14 08:25:25 -0300

    Update Conecte_4.java

--- IP/Conecte 4/src/Conecte_4.java
+++ IP/Conecte 4/src/Conecte_4.java
@@ -1,3 +1,5 @@
+// int rand = (int) Math.random() * 7;
+
 import java.util.Scanner;
 
 public class Conecte_4 {
@@ -11,21 +13,24 @@ public class Conecte_4 {
         char[][] tabuleiro = new char[6][7];
         char jogador = ' ';
         char computador = ' ';
-        boolean vitoria = false;
-        int posicao;
+        boolean fim = false;
+        int[] camadaColuna = {5, 5, 5, 5, 5, 5, 5};
 
         Criacao(tabuleiro);
         jogador = EscolhaCor(teclado);
-        if (jogador ==  'A') {
-            computador =  'V';
+        if (jogador == 'A') {
+            computador = 'V';
         } else {
             computador = 'A';
         }
 
         do {
-            posicao = EscolhaPosicao(teclado, tabuleiro, jogador, computador);
-            Jogada(posicao, tabuleiro, jogador, computador);
-        } while (!vitoria);
+            JogadaPlayer(teclado, tabuleiro, camadaColuna, jogador);
+            JogadaComputador(camadaColuna, tabuleiro, computador);
+            Output(tabuleiro);
+            fim = ValidarVitoria(camadaColuna);
+
+        } while (!fim);
 
         teclado.close();
     }
@@ -51,33 +56,62 @@ public class Conecte_4 {
         return op;
     }
 
-    private int EscolhaPosicao(Scanner teclado, char[][] tabuleiro, char jogador, char computador) {
-        int posicao = -1;
+    private void JogadaPlayer(Scanner teclado, char[][] tabuleiro, int[] camadaColuna, char jogador) {
+        int posicao;
         boolean check = false;
         do {
             System.out.println("\nEscolha a posição de jogada (entre 1 e 7): \n(0 para ver o tabuleiro)");
             posicao = teclado.nextInt();
             if (posicao == 0) {
-                System.out.println();
                 Output(tabuleiro);
             } else if (posicao >= 1 && posicao <= 7) {
+                posicao--;
+                if (camadaColuna[posicao] >= 0) {
+                    tabuleiro[camadaColuna[posicao]][posicao] = jogador;
+                    camadaColuna[posicao]--;
+                    check = true;
+                } else {
+                    System.out.println("\nJogada inválida. Coluna cheia.");
+                }
+            }
+        } while (!check);
+    }
+
+    private void JogadaComputador(int[] camadaColuna, char[][] tabuleiro, char computador) {
+        int posicaoComp = (int) (Math.random() * 7);
+        boolean check = false;
+        do {
+            if (camadaColuna[posicaoComp] >= 0) {
+                tabuleiro[camadaColuna[posicaoComp]][posicaoComp] = computador;
+                camadaColuna[posicaoComp]--;
                 check = true;
             }
         } while (!check);
-        return posicao;
-    }
-    
-    private void Jogada(int posicao, char[][] tabuleiro, char jogador, char computador) {
-
     }
 
     private void Output(char[][] tabuleiro) {
+        System.out.println();
         for (int linhas = 0; linhas < 6; linhas++) {
             for (int colunas = 0; colunas < 7; colunas++) {
                 System.out.print(tabuleiro[linhas][colunas] + " ");
-                ;
             }
             System.out.println();
         }
+    }
+
+    private boolean ValidarVitoria(int[] camadaColuna) {
+        int validarEmpate = 0;
+        for (int i = 0; i < camadaColuna.length; i++) {
+            if (camadaColuna[i] == -1) {
+                validarEmpate++;
+            }
+        }
+        if (validarEmpate == 7) {
+            System.out.println("\nEmpate.");
+            return true;
+        } else {
+            return false;
+        }
+    
     }
 }
