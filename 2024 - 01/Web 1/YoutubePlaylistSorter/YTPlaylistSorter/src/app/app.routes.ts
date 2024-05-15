753be747dceb55eff6a0b4929a6f8d56239219b1
Bernardo Chiamolera <bchiamolera@furb.br>
2024-05-15 16:44:54 -0300

    Limpando arquivos não utilizados

--- 2024 - 01/Web 1/YoutubePlaylistSorter/YTPlaylistSorter/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo:'dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent }
-];
